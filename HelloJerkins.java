378958bb2f82b57e66b7ac9a35ef6e81303d9a76
laxmi-karnati <laxmi.karnati2323@gmail.com>
2024-08-07 10:18:46 +0530

    Add files via upload

---
+++ HelloJerkins.java
@@ -0,0 +1,26 @@
+package demo;
+
+public class HelloJerkins {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		System.out.println("Jerkins");
+		
+		int x=20,y=20;
+		if(x<y)
+		{
+			System.out.println("x is less than y");
+		}else if(x==y) {
+			System.out.println("x is equal to y");
+		}else {
+		System.out.println("x is greater than y");
+		
+	
+	}
+		System.out.println("successfully executed");
+
+}
+
+
+	}
+
